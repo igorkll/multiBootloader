c329e5d22831312548cbaa2e1c8c36d2d276faea
igorkll <69525332+igorkll@users.noreply.github.com>
2022-08-22 18:05:45 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -10,6 +10,9 @@ if gpu and screen then
         gpu.setDepth(1)
         gpu.setDepth(gpu.maxDepth())
     end
+else
+    gpu = nil
+    screen = nil
 end
 
 ------------------------------------
@@ -25,3 +28,25 @@ local function clear()
     gpu.setForeground(0xFFFFFF)
     gpu.fill(1, 1, rx, ry, " ")
 end
+
+local function delay(time)
+    local inTime = computer.uptime()
+    while computer.uptime() - inTime < time do
+        computer.pullSignal(time - (computer.uptime() - inTime))
+    end
+end
+
+------------------------------------
+
+if gpu then
+    clear()
+    gpu.fill(1, 1, rx, ry, "+")
+    gpu.fill(2, 2, rx - 2, ry - 2, "+")
+end
+computer.beep(784)
+delay(0.25)
+computer.beep(784)
+delay(0.25)
+computer.beep(784)
+computer.beep(659)
+computer.beep(1047)
