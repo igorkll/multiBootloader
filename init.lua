84b26b5b5267bd205489fab6b9dc11f553f6c76a
igorkll <69525332+igorkll@users.noreply.github.com>
2022-08-22 18:13:22 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -17,11 +17,11 @@ end
 
 ------------------------------------
 
-bootdrive.makeDirectory("/operatingSystems")
-
-------------------------------------
-
 local rx, ry = gpu.getResolution()
+bootdrive.makeDirectory("/operatingSystems")
+local invoke = component.invoke
+local unpack = table.unpack
+local error = error
 
 local function clear()
     gpu.setBackground(0)
@@ -36,12 +36,20 @@ local function delay(time)
     end
 end
 
-------------------------------------
+local function drawMainLogo()
+    clear()
+    gpu.fill(1, 1, rx, ry, "@")
+    gpu.fill(2, 2, rx - 2, ry - 2, " ")
+    gpu.setBackground(0xFFFFFF)
+    gpu.setForeground(0)
+    gpu.fill(2, 2, rx - 2, 1, " ")
+    gpu.set(2, 2, "multi bootloader, use more one operating systems!")
+    gpu.setBackground(0)
+    gpu.setForeground(0xFFFFFF)
+end
 
 if gpu then
-    clear()
-    gpu.fill(1, 1, rx, ry, "+")
-    gpu.fill(2, 2, rx - 2, ry - 2, "+")
+    drawMainLogo()
 end
 computer.beep(784)
 delay(0.25)
@@ -50,3 +58,38 @@ delay(0.25)
 computer.beep(784)
 computer.beep(659)
 computer.beep(1047)
+
+------------------------------------
+
+function component.invoke(address, method, ...)
+    local args = {...}
+    if address == bootdrive.address then
+        if method == "open" then
+            
+        elseif method == "rename" then
+
+        elseif method == "remove" then
+
+        elseif method == "size" then
+            
+        elseif method == "" then
+        end
+    end
+    local result = {pcall(invoke(address, method, table.unpack(args)))}
+    if not result[1] then
+        error(result[2], 0)
+    end
+    return unpack(result, 2)
+end
+
+local function ()
+    
+end
+
+local function boot(name)
+    
+end
+
+while true do
+    
+end
