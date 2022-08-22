e3250efcdc0c97e251f5b787d5aaab1a018647b3
igorkll <69525332+igorkll@users.noreply.github.com>
2022-08-22 17:59:17 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,15 @@
 local gpu = component.proxy((computer.getBootGpu() or component.list("gpu")()) or "")
 local screen = computer.getBootScreen() or component.list("screen")()
+local bootdrive = computer.getBootAddress()
 
 if gpu and screen then
     if not gpu.getScreen() then
         gpu.bind(screen)
     end
 end
+
+------------------------------------
+
+bootdrive.makeDirectory("/operatingSystems")
 
 ------------------------------------
